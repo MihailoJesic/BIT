d9ab781615949d8e44f3e9c6f7948573a561b62d
mikash91 <mihailo_jesic@yahoo.com>
2022-11-02 15:14:57 +0100

    Closure

--- Week4/flex2/script.js
+++ Week4/flex2/script.js
@@ -1,1 +1,23 @@
-`use strict`
+`use strict`;
+
+// function dino() {
+//   var counter = 0;
+//   let bla = `bla`;
+//   function dodaj() {
+//     counter++;
+//     counter2 = 1;
+//     counter2++;
+//     console.log(counter, counter2, bla);
+//   }
+//   return dodaj;
+// }
+
+// let novo = dino();
+
+// novo();
+// novo();
+// novo();
+// novo();
+// novo();
+
+// console.log(counter);
