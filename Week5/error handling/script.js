7ad962c15ffa70534f5346bcb92ede69ca3e5c91
mikash91 <mihailo_jesic@yahoo.com>
2022-11-10 09:49:10 +0100

    Error Handling

--- Week5/error handling/script.js
+++ Week5/error handling/script.js
@@ -1,1 +1,14 @@
 `use strict`;
+let condition = false;
+
+try {
+  if (!condition) {
+    throw new Error(`❌Big Problem❌`);
+  } else {
+    console.log(`All's well`);
+  }
+} catch (error) {
+  console.error(error);
+} finally {
+  console.log(`Still chugging along`);
+}
