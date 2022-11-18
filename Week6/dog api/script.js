a7c278ceeaa127503ceb9f2a3233ecbaa9beabf8
mikash91 <mihailo_jesic@yahoo.com>
2022-11-18 15:15:57 +0100

    all tibetan terriers

--- Week6/dog api/script.js
+++ Week6/dog api/script.js
@@ -1,7 +1,8 @@
 "use strict";
 
-let img = document.querySelector(`img`);
-let btn = document.querySelector(`a`);
+// let img = document.querySelector(`img`);
+// let btn = document.querySelector(`a`);
+let div = document.querySelector(`div`);
 
 async function dog() {
   let data = await fetch(
@@ -12,6 +13,21 @@ async function dog() {
   img.src = url;
 }
 
-dog();
+// dog();
+
+async function allDogs() {
+  let res = await fetch(`https://dog.ceo/api/breed/terrier/tibetan/images`);
+  // console.log(`res >> `, res);
+  let data = await res.json();
+  // console.log(`res >> `, data);
+  let result = data.message;
+  // console.log(`res >> `, result);
+
+  result.forEach((el) => {
+    div.innerHTML += `<img src="${el}">\n`;
+  });
+}
+
+allDogs();
 
 btn.addEventListener(`click`, dog);
