c3039cecc334f909f5b8203e190c48a7864cbba3
mikash91 <mihailo_jesic@yahoo.com>
2022-11-24 16:03:31 +0100

    state

--- week7/react-project/src/components/main/main.js
+++ week7/react-project/src/components/main/main.js
@@ -3,15 +3,32 @@ import "./main.css";
 import Card from "../card/card";
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 1,
+    };
+  }
   arr = [1, 2, 3, 4, 5, 6, 7, 8];
 
-  render(props) {
+  render() {
     return (
       <>
         <main>
-          {this.arr.map((el) => {
-            return <Card val={el} />;
-          })}
+          <h2>Number is {this.state.number}</h2>
+          <button
+            onClick={() => {
+              this.setState({ number: this.state.number + 1 });
+              this.render();
+            }}
+          >
+            Increase Number
+          </button>
+          <div>
+            {this.arr.map((el) => {
+              return <Card val={el} />;
+            })}
+          </div>
         </main>
       </>
     );
