64242c5867369fb8300797621c53c9a26e01f374
mikash91 <mihailo_jesic@yahoo.com>
2022-11-30 11:47:59 +0100

    functional

---
+++ week8/functional/src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import comp from "./comp";
+
+function App() {
+  return comp();
+}
+
+export default App;
---
+++ week8/functional/src/comp.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+
+const comp = () => {
+  return (
+    <div>
+      <h1>Welcome</h1>
+    </div>
+  );
+};
+
+export default comp;
