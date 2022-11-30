c69af9f0a60240aab4824184e3308ca38d839866
mikash91 <mihailo_jesic@yahoo.com>
2022-11-30 14:59:52 +0100

    functional

--- week8/functional/src/App.js
+++ week8/functional/src/App.js
@@ -1,13 +1,19 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
 import Button from "./button";
 
 function App() {
   const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${counter} times`;
+  });
+
   return (
     <>
       <div className="container">
-        <h2>{counter}</h2>
+        <h2>Your number is {counter}</h2>
         <Button methods={setCounter} counter={counter}></Button>
       </div>
     </>
